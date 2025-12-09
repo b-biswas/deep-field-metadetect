f7572562f5bd6bd0a441e654725bfa6690827c4b
Biswajit Biswas <bbiswas.dgp@gmail.com>
2025-12-09 00:51:21 -0600

    minor

--- deep_field_metadetect/tests/test_metadetect.py
+++ deep_field_metadetect/tests/test_metadetect.py
@@ -45,7 +45,7 @@ def _run_single_sim(
 
         pdb.set_trace()
 
-    res, _ = single_band_deep_field_metadetect(
+    res = single_band_deep_field_metadetect(
         obs_w,
         obs_d,
         obs_dn,
@@ -103,7 +103,7 @@ def test_metadetect_single_band_deep_field_metadetect_bmask():
     )
     obs_w.bmask = rng.choice([0, 1, 3], p=[0.5, 0.25, 0.25], size=obs_w.image.shape)
 
-    res, _ = single_band_deep_field_metadetect(
+    res = single_band_deep_field_metadetect(
         obs_w,
         obs_d,
         obs_dn,
@@ -140,15 +140,13 @@ def test_metadetect_single_band_deep_field_metadetect_mfrac_wide():
     )
     obs_w.mfrac = rng.uniform(0.5, 0.7, size=obs_w.image.shape)
 
-    res, kinfo = single_band_deep_field_metadetect(
+    res = single_band_deep_field_metadetect(
         obs_w,
         obs_d,
         obs_dn,
         skip_obs_wide_corrections=False,
         skip_obs_deep_corrections=False,
     )
-
-    assert kinfo is None
 
     msk = (res["wmom_flags"] == 0) & (res["mdet_step"] == "noshear")
     assert np.all(res["mfrac"][msk] >= 0.5)
@@ -173,7 +171,7 @@ def test_metadetect_single_band_deep_field_metadetect_mfrac_deep():
     )
     obs_d.mfrac = rng.uniform(0.5, 0.7, size=obs_w.image.shape)
 
-    res, kinfo = single_band_deep_field_metadetect(
+    results = single_band_deep_field_metadetect(
         obs_w,
         obs_d,
         obs_dn,
@@ -185,6 +183,9 @@ def test_metadetect_single_band_deep_field_metadetect_mfrac_deep():
         force_stepk_psf=0.6815071326229606,
         force_maxk_psf=12.640001692177682,
     )
+
+    res = results["dfmdet_res"]
+    kinfo = results["kinfo"]
 
     assert kinfo[0] == 0.12403490725241548
     assert kinfo[1] == 8.160777791551611
